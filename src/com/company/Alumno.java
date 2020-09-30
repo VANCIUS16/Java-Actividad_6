da8603f4f4175df681bb9df60fac8c198495e114
unknown <marcnadal7@gmail.com>
2020-09-29 22:52:30 -0500

    Agregue anotaciónes y cambio de varibales de publicas a privadas

--- src/com/company/Alumno.java
+++ src/com/company/Alumno.java
@@ -3,34 +3,40 @@ package com.company;
 import java.util.Scanner;
 
 public class Alumno {
+
+    //Se declara la función donde se ingresan los valores
     static Scanner teclado = new Scanner(System.in);
 
-    public String nombre;
-    public double[] cali = new double[5];
-    public double prom;
+    //Declaración de varubales
+    private String nombre;
+    private double[] cali = new double[5];
     public double cont;
-    public double promf;
-    public double cont2;
-    public char letra;
+    private double promf;
+    private double cont2;
+    private char letra;
 
+    //Inicialización de las variables
     public Alumno() {
-        cont = 0;
-        prom = 0;
-        promf = 0;
-        cont2 = 0;
+        promf  = 0;
+        cont2  = 0;
+        cont   = 0;
         nombre = null;
-        letra = 0;
+        letra  = 0;
     }
+    //Método que guarda el valor del nombre y el valor del arreglo de calificaciónes
     public String Nombre(String nombre, double[] cali) {
         this.nombre = nombre;
         this.cali = cali;
 
         return nombre;
     }
+    //Metodo que realiza las matematicas para sacar el Promedio
     public double calif(double cont)
     {
         return cont/cali.length;
     }
+
+    //Método que retorna la letra de la calificacón
     public char Calificaciones(double promf) {
         this.promf = promf;
 
@@ -54,6 +60,7 @@ public class Alumno {
         }
         return letra;
     }
+    //Método que imprime el resultado
     public void print(String nombre, double promf, char letra)
     {
         System.out.println("Nombre: " + nombre);
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,19 +8,23 @@ public class Main
     
     public static void main(String[] args)
     {
+        // Declaracion de variables
         double [] cali = new double[5];
         double cont = 0;
         String nombre;
 
+        //Se inserta el nombre
         System.out.println("Ingrese nombre del Alumno: ");
         nombre = teclado.nextLine();
 
+        //Se ingresan las calificacónes
         System.out.println("Ingrese las calificaciónes: ");
         for (int x = 0; x < 5; x++) {
             cali[x] = teclado.nextDouble();
             cont =+ cali[x];
         }
 
+        //Se
         Alumno al = new Alumno();
         al.Nombre(nombre, cali);
         al.calif(cont);
